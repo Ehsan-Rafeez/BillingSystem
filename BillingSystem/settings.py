aabaaee253b6f8bfb0aa6255ea401dc9a77159d3
mirzaatif89 <mirzaatif7809@gmail.com>
2025-09-26 18:49:14 +0500

    Order list inline added

--- BillingSystem/settings.py
+++ BillingSystem/settings.py
@@ -82,16 +82,14 @@ WSGI_APPLICATION = 'BillingSystem.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME', ),     
-        'USER': config('DB_USER', ),            
-        'PASSWORD':   '',            
-        'HOST': '127.0.0.1',        
-        'PORT': '3306',             
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
+
 
 
 # Password validation
